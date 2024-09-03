2cb9d9d0d858b0f7cfec2c3edb2db4228fe7a858
nave-nurihaus <nave@nurihaus.com>
2024-09-03 15:40:02 +0900

    🔥 remove for pr

--- src/components/common/Gap.tsx
+++
@@ -1,7 +0,0 @@
-import {View} from 'react-native';
-
-export const Gap = ({size}: {size: number}) => {
-  return (
-    <View style={{width: '100%', height: size, backgroundColor: '#ddd'}} />
-  );
-};
--- src/utils/date.ts
+++
@@ -1,3 +0,0 @@
-export const getSlicedDateByMonth = (original?: string) => {
-  return original?.split('-')[0] + '-' + original?.split('-')[1];
-};
